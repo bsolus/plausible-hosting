dcfd9b2d030f9c45dce078b13215c9916da33983
Márcio Brandão <mpadinhabrandao@gmail.com>
2023-03-17 16:16:26 +0000

    IN-1230

--- backups/backup-databases.sh
+++ backups/backup-databases.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /var/docker/plausible/
+
 docker compose exec plausible_events_db bash /backups/clickhouse-2d-rotate.sh
 
 docker compose exec plausible_db bash /backups/postgres-2d-rotate.sh
